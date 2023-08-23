b03e50f1a138e4950c04534974abfed82a0d3ec2
Gathsara2002 <liviru2002mmg@gmail.com>
2023-08-23 20:58:50 +0530

    update do get method to take item data from DB

--- WebPOS/src/servlet/PlaceOrderServlet.java
+++ WebPOS/src/servlet/PlaceOrderServlet.java
@@ -55,5 +55,37 @@ public class PlaceOrderServlet extends HttpServlet {
             e.printStackTrace();
         }
 
+        /*get item details*/
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/webPos", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("select * from Item");
+            ResultSet resultSet = pstm.executeQuery();
+
+            resp.addHeader("Content-Type", "application/json");
+
+            JsonArrayBuilder allItems = Json.createArrayBuilder();
+
+            while (resultSet.next()) {
+                String code = resultSet.getString(1);
+                String name = resultSet.getString(2);
+                double price = resultSet.getDouble(3);
+                double qty = resultSet.getDouble(4);
+
+                JsonObjectBuilder itemObj = Json.createObjectBuilder();
+                itemObj.add("code", code);
+                itemObj.add("name", name);
+                itemObj.add("price", price);
+                itemObj.add("qty", qty);
+
+                allItems.add(itemObj.build());
+            }
+
+            resp.getWriter().print(allItems.build());
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
