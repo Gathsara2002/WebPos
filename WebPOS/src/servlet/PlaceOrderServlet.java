1b80e1296b6c01bc205cbe228222df4bc0a44f00
Gathsara2002 <liviru2002mmg@gmail.com>
2023-08-23 20:39:42 +0530

    create PlaceOrderServlet

---
+++ WebPOS/src/servlet/PlaceOrderServlet.java
@@ -0,0 +1,12 @@
+/**
+ * @author : Gathsara
+ * created : 8/23/2023 -- 8:38 PM
+ **/
+
+package servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class PlaceOrderServlet extends HttpServlet {
+
+}
