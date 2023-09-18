7d050baaf5db303a131badedad589e988fa7e4cd
Gathsara2002 <liviru2002mmg@gmail.com>
2023-09-18 20:25:57 +0530

    added filter class to back end

---
+++ Back_End/src/filter/MyFilter.java
@@ -0,0 +1,25 @@
+package filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author : Gathsara
+ * created : 9/18/2023 -- 8:24 PM
+ **/
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
