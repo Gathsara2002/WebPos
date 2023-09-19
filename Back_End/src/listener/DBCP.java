b9c5739275bb0e56a641268ee314b5e027cda45a
Gathsara2002 <liviru2002mmg@gmail.com>
2023-09-19 20:19:51 +0530

    added listener to back end

---
+++ Back_End/src/listener/DBCP.java
@@ -0,0 +1,31 @@
+package listener;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @author : Gathsara
+ * created : 9/19/2023 -- 8:14 PM
+ **/
+public class DBCP implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        BasicDataSource pool = new BasicDataSource();
+        pool.setDriverClassName("com.mysql.jdbc.Driver");
+        pool.setUrl("jdbc:mysql://localhost:3306/webPos");
+        pool.setUsername("root");
+        pool.setPassword("1234");
+        pool.setInitialSize(10);
+        pool.setMaxTotal(10);
+        servletContext.setAttribute("dbcp", pool);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
