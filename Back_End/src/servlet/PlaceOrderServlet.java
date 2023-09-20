3c8a62a8c7ae8a65061d3620e1ead15ea4e65a73
Gathsara2002 <liviru2002mmg@gmail.com>
2023-09-20 17:00:26 +0530

    added dbcp to placeorder servlet

--- Back_End/src/servlet/PlaceOrderServlet.java
+++ Back_End/src/servlet/PlaceOrderServlet.java
@@ -5,10 +5,23 @@
 
 package servlet;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
 
 
 @WebServlet(urlPatterns = "/placeOrder")
 public class PlaceOrderServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = getServletContext();
+        Connection connection = (Connection) servletContext.getAttribute("dbcp");
+
+
+    }
 }
